55290450dbddba076824741f118be56fa77f866b
Otsuka Satoshi <satoshi7108@gmail.com>
2020-06-21 18:23:47 +0900

    added config

---
+++ config/site-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'foobar',
+  description: 'note to self',
+  url: 'https://blog.otsukasatoshi.com',
+  author: 'Otsuka Satoshi',
+  avatar: 'avatar.png',
+  defaultImg: 'default.png',
+  shortBio: 'Web developer.',
+  portfolio: 'https://otsukasatoshi.com',
+  twitter: 'https://twitter.com/otsukasatoshi',
+  instagram: 'https://www.instagram.com/by_otsukasatoshi',
+  github: 'https://github.com/otsukasatoshi',
+  userName: 'otsukasatoshi'
+};
---
+++ config/site-labels.js
@@ -0,0 +1,9 @@
+module.exports = {
+  home: 'Home',
+  notFound: 'Not Found Page',
+  prev: '前へ',
+  next: '次へ',
+  toc: 'Table Of Contents',
+  searchLabel: 'search',
+  searchResult: '検索結果'
+};
