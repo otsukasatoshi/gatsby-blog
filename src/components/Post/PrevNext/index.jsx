4f225deaf294e4956e6f78c84b05dadd7de03c81
Otsuka Satoshi <satoshi7108@gmail.com>
2020-06-21 21:23:20 +0900

    added PrevNext component

---
+++ src/components/Post/PrevNext/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Label from '../../../../config/site-labels';
+import './style.scss';
+
+const PrevNext = ({ previous, next }) => {
+  return (
+    <ul className="prev-next">
+      <li>
+        {previous && (
+          <Link to={previous.fields.slug} rel="prev">
+            <span>← {Label.prev}</span>
+            <span>{previous.frontmatter.title}</span>
+          </Link>
+        )}
+      </li>
+      <li>
+        {next && (
+          <Link to={next.fields.slug} rel="next">
+            <span>{Label.next} →</span>
+            <span>{next.frontmatter.title}</span>
+          </Link>
+        )}
+      </li>
+    </ul>
+  );
+};
+
+export default PrevNext;
