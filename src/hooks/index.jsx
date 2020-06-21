43e1945444f12c75c5ab7dd1ed013bffcbc888ab
Otsuka Satoshi <satoshi7108@gmail.com>
2020-06-21 17:55:27 +0900

    added hooks index.jsx

---
+++ src/hooks/index.jsx
@@ -0,0 +1,3 @@
+export { default as useSiteMetadata } from './use-site-metadata';
+export { default as useImagePath } from './use-image-path';
+export { default as useActiveHash } from './use-active-hash';
