89d0d8d219401160b4fe0c6b8e6429e471323f04
Otsuka Satoshi <satoshi7108@gmail.com>
2020-07-03 22:12:29 +0900

    added sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -104,6 +104,37 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        query: `
+        {
+          site {
+            siteMetadata {
+              siteUrl
+            }
+          }
+          allSitePage(
+            filter: {
+              path: { regex: "/^(?!/404/|/404.html|/dev-404-page/|/offline-plugin-app-shell-fallback/)/" }
+            }
+          ) {
+            edges {
+              node {
+                path
+              }
+            }
+          }
+        }
+        `,
+        output: '/sitemap.xml',
+        serialize: ({ site, allSitePage }) => allSitePage.edges.map((edge) => ({
+          url: site.siteMetadata.siteUrl + edge.node.path,
+          changefreq: 'daily',
+          priority: 0.7,
+        }))
+      },
+    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-offline',
