1ece3d71ebecb332af4fd721be72e3136c18b35e
Otsuka Satoshi <satoshi7108@gmail.com>
2020-08-15 22:30:33 +0900

    fix PWA(refresh the page when service worker finds an update)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,6 @@ import 'typeface-montserrat';
 import 'typeface-notosans-jp';
 import './src/styles/app.scss';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+
+// refresh the page when service worker finds an update
+export const onServiceWorkerUpdateReady = () => window.location.reload(true);
